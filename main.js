483de09e93b92b14750e20fe2618645339a66b8a
siqueira123 <122622906+siqueira123@users.noreply.github.com>
2023-04-27 11:27:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-var images=["Donatello.jpg","Leonardo.jpg","Michelangelo.jpg","Raphael.jpg"]
+var images=["https://i.postimg.cc/yYmz85p3/familia.jpg","https://i.postimg.cc/DZMMxQjT/Donatello.jpg","https://i.postimg.cc/4x17VWym/Leonardo.jpg","https://i.postimg.cc/xTfSq5ws/Michelangelo.jpg","https://i.postimg.cc/KjYJh1W5/Raphael.jpg"]
 var names=["Donatelo","Leonardo","Michelangelo","Raphael"]
 
 var i = 0;
 function update()
 {
-    i++
-
     var numbersOffFamilyInArray = 5
     if(i > numbersOffFamilyInArray)
     {
@@ -16,4 +14,5 @@ function update()
     var updateNames = [i];
     document.getElementById("ImagemDosMembrosDaFamilia").src = updateImages;
     document.getElementById("nomeDosMembrosDaFamilia").innerHTML = updateNames;
+    i++
 }
